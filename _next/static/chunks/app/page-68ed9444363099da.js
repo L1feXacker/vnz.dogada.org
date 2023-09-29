bd79c9afc725ecdd823bcfdac0fa04c61af9ab89
Stas L1feXacker <stas007sm@gmail.com>
2023-09-29 17:49:26 +0300

    New version

--- _next/static/chunks/app/page-68ed9444363099da.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,968,451,260],{2635:function(n,e,u){Promise.resolve().then(u.t.bind(u,9101,23))}},function(n){n.O(0,[101,702,431,744],function(){return n(n.s=2635)}),_N_E=n.O()}]);
